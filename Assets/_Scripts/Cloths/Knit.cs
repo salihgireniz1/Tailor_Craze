e21e95a61de9b6493d696b04609faeeccb69ac34
Salih Gireniz <salihgireniz1@gmail.com>
2024-11-17 01:19:51 +0300

    .

--- Assets/_Scripts/Cloths/Knit.cs
+++ Assets/_Scripts/Cloths/Knit.cs
@@ -4,9 +4,9 @@ using Cysharp.Threading.Tasks;
 
 public abstract class Knit : MonoBehaviour
 {
-    public abstract Material KnitMaterial{get; set;}
+    public abstract Material KnitMaterial { get; set; }
     // public float _activitionDuration = 0.75f;
-    public float targetScale = 0.15f;
+    public float targetScale = 1f;
     public Ease _activitionEasing = Ease.OutBack;
     public abstract void InitializeKnit(Color32 color);
 
