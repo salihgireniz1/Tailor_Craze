af5009b9f79a21a7e4c56f83e75d3bf251cccbfa
Salih Gireniz <salihgireniz1@gmail.com>
2024-11-19 00:03:53 +0300

    cloth count and hand pos

--- Assets/_Scripts/UI/ClothProgressCounter.cs
+++ Assets/_Scripts/UI/ClothProgressCounter.cs
@@ -7,14 +7,10 @@ public class ClothProgressCounter : MonoBehaviour
     [SerializeField] private TextMeshProUGUI counterText;
     private int _current;
     private int _total;
-    private void OnEnable()
-    {
-        _current = ClothsController.Instance.ClothCount.Value;
-        _total = ClothsController.Instance.LevelClothsCount.Value;
-        counterText.text = $"{_current}/{_total}";
-    }
     void Start()
     {
+        counterText.text = $"{ClothsController.Instance.ClothCount.Value}/{ClothsController.Instance.LevelClothsCount.Value}";
+
         ClothsController.Instance.ClothCount
         .Subscribe(c =>
         {
