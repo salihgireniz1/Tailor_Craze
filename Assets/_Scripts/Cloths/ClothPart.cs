eb69ba06dd9e3c17c4cc38df442e3e893bdea998
Salih Gireniz <salihgireniz1@gmail.com>
2024-11-21 12:24:40 +0300

    Update ClothPart.cs

--- Assets/_Scripts/Cloths/ClothPart.cs
+++ Assets/_Scripts/Cloths/ClothPart.cs
@@ -92,7 +92,7 @@ public class ClothPart : MonoBehaviour, IFillable, IConnect
             {
                 SoundManager.Instance.PlaySFX(SFXType.Knitted);
                 hapticCounter++;
-                if (hapticCounter % 3 == 0) HapticManager.HapticPlay(HapticType.VibratePop);
+                if (hapticCounter % 2 == 0) HapticManager.HapticPlay(HapticType.VibratePop);
 
                 await currentKnit.Activate(knitDuration);
             }
