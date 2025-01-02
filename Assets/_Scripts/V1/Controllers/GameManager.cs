dc6fc17e9d211afe1837d5b8c587d299e6a24d8f
berkterek <42406263+berkterek@users.noreply.github.com>
2025-01-02 13:45:14 +0300

    refactor: homa events added

--- Assets/_Scripts/V1/Controllers/GameManager.cs
+++ Assets/_Scripts/V1/Controllers/GameManager.cs
@@ -1,3 +1,4 @@
+using HomaGames.HomaBelly;
 using R3;
 using UnityEngine;
 public enum GameState
@@ -22,4 +23,22 @@ public class GameManager : MonoSingleton<GameManager>
         CurrentState.Value = GameState.Initializing;
     }
 
+    void Start()
+    {
+        CurrentState.Subscribe((state) =>
+        {
+            switch (state)
+            {
+                case GameState.Victory:
+                    Analytics.LevelCompleted();
+                    break;
+                case GameState.GameOver:
+                    Analytics.LevelFailed(string.Empty, null);
+                    break;
+                case GameState.Initializing:
+                    Analytics.LevelStarted(LevelManager.Level);
+                    break;
+            }
+        }).AddTo(this);
+    }
 }
